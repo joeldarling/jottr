6ed2dad9a7cc7f1855f7d626cd00bcf7a458bc1b
Joel <joel.darling@gmail.com>
2016-07-19 18:02:50 -0400

    testing

--- public/js/home/home.js
+++ public/js/home/home.js
@@ -3,18 +3,22 @@
 app.controller('HomeCtrl', function($scope, $interval){
 
   $scope.timer = 10;
+  $scope.data = {savedLines: []};
 
-  var stop;
-
-  $interval(function(){
+  var stop = $interval(function(){
     $scope.timer--;
 
     if($scope.timer === 0)
-      alert('your time is up');
-
+      $interval.cancel(stop);
   }, 1000);
 
   $scope.changed = function($event){
+
     $scope.timer = 10;
+
+    if($event.keyCode == '13' && $event.shiftKey){
+      $scope.data.savedLines.push($scope.data.text);
+      $scope.data.text = undefined;
+    }
   };
 });

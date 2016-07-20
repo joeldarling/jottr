c5fb9a134737d1c70d890d87ea4b01c3d7526e39
Joel <joel.darling@gmail.com>
2016-07-20 19:14:31 -0400

    adding text analytics

--- public/js/home/home.js
+++ public/js/home/home.js
@@ -4,6 +4,8 @@ app.controller('HomeCtrl', function($scope, $interval){
 
   $scope.timer = 10;
   $scope.data = {savedLines: []};
+  $scope.stats;
+
 
   var stop = $interval(function(){
     $scope.timer--;
@@ -16,9 +18,16 @@ app.controller('HomeCtrl', function($scope, $interval){
 
     $scope.timer = 10;
 
-    if($event.keyCode == '13' && $event.shiftKey){
+    if($scope.stats.words > 10 && $event.keyCode == '13' && $event.shiftKey){
       $scope.data.savedLines.push($scope.data.text);
       $scope.data.text = undefined;
     }
+
   };
+
+  //set up the word stats
+  Countable.live(document.getElementById('writer-input'), function (counter) {
+    $scope.stats = counter;
+  });
+
 });
--- server/index.js
+++ server/index.js
@@ -3,18 +3,22 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
 
 /// SETUP MIDDLEWARE ///
 app.use(bodyParser.json()); // for parsing application/json
 
 /// START SERVER ///
-app.listen(3000, function(){
+server.listen(3000, function(){
   console.log('server started on port 3000');
 });
 
 /// SETUP STATIC ROUTES ///
 app.use('/js', express.static(path.join(__dirname, '../public/js')));
 app.use('/node_modules', express.static(path.join(__dirname, '../node_modules')));
+app.use('/bower_components', express.static(path.join(__dirname, '../bower_components')));
 
 
 app.get('/*', function (req, res) {

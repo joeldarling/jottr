cf580b2ef9aa4d3aad3e2b7697b601688f3cfaa7
Joel <joel.darling@gmail.com>
2016-07-20 22:58:49 -0400

    ui work

--- public/js/app.js
+++ public/js/app.js
@@ -2,14 +2,19 @@ var app = angular.module('suddendeath', ['ui.router','ngMaterial','angular-clipb
 
 var socket = io.connect();
 
-
-app.config(function ($urlRouterProvider, $locationProvider) {
+app.config(function ($urlRouterProvider, $locationProvider, $mdThemingProvider) {
 
     // This turns off hashbang urls (/#about) and changes it to something normal (/about)
     $locationProvider.html5Mode(true);
     // If we go to a URL that ui-router doesn't have registered, go to the "/admin" url.
     $urlRouterProvider.otherwise('/');
 
+    $mdThemingProvider.theme('default')
+     .primaryPalette('grey')
+     .accentPalette('teal', {
+       default:'A400'
+     })
+     .dark();
 });
 
 app.config(function ($stateProvider) {

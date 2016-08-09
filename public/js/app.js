e37b1739b6a61d55399f7e35f17f0cb3f2c6011d
Joel <joel.darling@gmail.com>
2016-08-09 19:01:02 -0400

    updated default route

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ app.config(function ($urlRouterProvider, $locationProvider, $mdThemingProvider, 
     // This turns off hashbang urls (/#about) and changes it to something normal (/about)
     $locationProvider.html5Mode(true);
     // If we go to a URL that ui-router doesn't have registered, go to the "/admin" url.
-    $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/write');
 
     hotkeysProvider.useNgRoute = false;
 
